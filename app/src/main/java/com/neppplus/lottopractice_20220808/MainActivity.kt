3e519a4c6abddd8d75fc27aa7f6c85184c768c91
넵플러스 <kmc901206@gmail.com>
2022-08-08 18:23:56 +0900

    당첨 확인 로직 작성 중 > 텍스트뷰에 데이터 반영

--- app/src/main/java/com/neppplus/lottopractice_20220808/MainActivity.kt
+++ app/src/main/java/com/neppplus/lottopractice_20220808/MainActivity.kt
@@ -17,9 +17,16 @@ class MainActivity : AppCompatActivity() {
 //    랜덤 번호 6개를 집어넣을 텍스트뷰 자료형의 ArrayList를 만들자
     val mWinNumViewList = ArrayList<TextView>()
 
-//    사용금액 / 당첨금액
+//    사용금액 / 당첨금액 / 당첨 횟수
     var mUsedMoney = 0
     var mWinMoney = 0L  // 30억 이상의 당첨 대비, Long 타입으로 설정
+
+    var firstCount = 0
+    var secondCount = 0
+    var thirdCount = 0
+    var fourthCount = 0
+    var fifthCount = 0
+    var loseCount = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -79,9 +86,66 @@ class MainActivity : AppCompatActivity() {
 //            for-each문 역시 index값을 같이 활용 for문 작성 > .withIndex()
             mWinNumViewList[index].text = num.toString()
         }
+
+//        3. 보너스 번호 하나 선정 > 텍스트 뷰에 배치
+        while (true) {
+            val randomNum = (Math.random() * 45 + 1).toInt()
+
+            if (!mWinNumList.contains(randomNum)) {
+                mBonusNum = randomNum
+                bonusNumTxt.text = mBonusNum.toString()
+                break
+            }
+        }
+
+        checkLotto()
     }
 
     fun checkLotto() {
+//        4. 비교
+        var correctCount = 0
+
+//        내 번호를 하나씩 조회 (for each)
+        for (myNum in mMyNumList) {
+//            당첨 번호를 맞췄는가? => 당첨번호 목록에 내 번호가 들어있나?
+            if (mWinNumList.contains(myNum)) {
+                correctCount++
+            }
+        }
+
+//        5. 순위 선정 (맞춘 갯수에 따라) > 등수 판단 // 텍스뷰에 출력
+        when (correctCount) {
+            6 -> {
+//                1등 당첨
+                mWinMoney += 3000000000
+                firstCount++
+            }
+            5 -> {
+//                보너스 번호를 맞췄는지 => 보너스 번호가 내 번호 목록에 들어있나?
+                if (mMyNumList.contains(mBonusNum)) {
+//                    가지고 있다 > 2등
+                    mWinMoney += 50000000
+                    secondCount ++
+                }
+                else {
+                    mWinMoney += 2000000
+                    thirdCount ++
+                }
+            }
+            4 -> {
+                mWinMoney += 50000
+                fourthCount ++
+            }
+            3 -> {
+                mWinMoney += 5000
+                fifthCount ++
+            }
+            else -> {
+                loseCount ++
+            }
+        }
+
+//        사용금액 / 당첨 금액 및 당첨 횟수를 텍스트뷰에 각각 반영
 
     }
 

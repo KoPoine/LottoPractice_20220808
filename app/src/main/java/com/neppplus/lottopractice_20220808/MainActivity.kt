72e954c3add9bb7bdc137665f740ffbcdcab532b
넵플러스 <kmc901206@gmail.com>
2022-08-09 10:47:30 +0900

    로또 프로젝트 > 자동 추첨 기능 Add

--- app/src/main/java/com/neppplus/lottopractice_20220808/MainActivity.kt
+++ app/src/main/java/com/neppplus/lottopractice_20220808/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.neppplus.lottopractice_20220808
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.util.Log
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -21,16 +25,22 @@ class MainActivity : AppCompatActivity() {
     var mUsedMoney = 0
     var mWinMoney = 0L  // 30억 이상의 당첨 대비, Long 타입으로 설정
 
-    var firstCount = 0
-    var secondCount = 0
-    var thirdCount = 0
-    var fourthCount = 0
-    var fifthCount = 0
-    var loseCount = 0
+    var mFirstCount = 0
+    var mSecondCount = 0
+    var mThirdCount = 0
+    var mFourthCount = 0
+    var mFifthCount = 0
+    var mLoseCount = 0
+
+    var isAuto = false
+
+    lateinit var mHandler : Handler
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mHandler = Handler(Looper.getMainLooper())
 
         mWinNumViewList.add(winNum1Txt)
         mWinNumViewList.add(winNum2Txt)
@@ -46,11 +56,53 @@ class MainActivity : AppCompatActivity() {
 
         autoBuyBtn.setOnClickListener {
 
+            if (!isAuto) {
+//            처음 눌리면 > 반복 구매 시작  => 이 버튼이 처음눌렸는지? 아닌지에 대한 정보
+
+//                단순 반복 => UI 갱신속도가 못따라온다 (사용자의 입장에서 앱이 죽은 것 처럼 보인다.)
+//                while (true) {
+//                    buyLotto()
+//
+//                    if (mUsedMoney >= 50000000) {
+//                        break
+//                    }
+//                }
+                isAuto = true
+                mHandler.post(buyLottoRunnable)
+            }
+            else {
+//            다시 눌리면 > 반복 구매 정지 > 핸들러로 동작
+//                핸들러에게 등록된 다음 할 일(buyLottoRunnable) 제거
+                mHandler.removeCallbacks(buyLottoRunnable)
+
+                isAuto = false
+            }
+
+
+
+        }
+    }
+
+    val buyLottoRunnable  = object : Runnable{
+        override fun run() {
+//            1000만원을 아직 안썻다면 > 로또 한장을 추가 구매
+            if (mUsedMoney <= 10000000) {
+                buyLotto()
+
+//            핸들러에게 다시 로또 한장을 사는 행위(buyLottonRunnable)를 등록 (재귀함수)
+                mHandler.post(this)
+            }
+//            1000만원이 넘었다면 할 일 정지
+            else {
+                Toast.makeText(this@MainActivity, "자동 구매가 완료되었습니다.", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 
     fun buyLotto() {
         mUsedMoney += 1000
+
+        mWinNumList.clear()
 
 //        1. 로또 당첨 번호 6개 선정
         for (i in 0 until 6) {
@@ -118,35 +170,43 @@ class MainActivity : AppCompatActivity() {
             6 -> {
 //                1등 당첨
                 mWinMoney += 3000000000
-                firstCount++
+                mFirstCount++
             }
             5 -> {
 //                보너스 번호를 맞췄는지 => 보너스 번호가 내 번호 목록에 들어있나?
                 if (mMyNumList.contains(mBonusNum)) {
 //                    가지고 있다 > 2등
                     mWinMoney += 50000000
-                    secondCount ++
+                    mSecondCount ++
                 }
                 else {
                     mWinMoney += 2000000
-                    thirdCount ++
+                    mThirdCount ++
                 }
             }
             4 -> {
                 mWinMoney += 50000
-                fourthCount ++
+                mFourthCount ++
             }
             3 -> {
                 mWinMoney += 5000
-                fifthCount ++
+                mFifthCount ++
             }
             else -> {
-                loseCount ++
+                mLoseCount ++
             }
         }
 
 //        사용금액 / 당첨 금액 및 당첨 횟수를 텍스트뷰에 각각 반영
+        usedMoneyTxt.text = "사용금액 : ${mUsedMoney}원"
+        winMoneyTxt.text = "당첨금액 : ${mWinMoney}원"
 
+        rank1stTxt.text = "1등 당첨 횟수 : ${mFirstCount}회"
+        rank2ndTxt.text = "2등 당첨 횟수 : ${mSecondCount}회"
+        rank3rdTxt.text = "3등 당첨 횟수 : ${mThirdCount}회"
+        rank4thTxt.text = "4등 당첨 횟수 : ${mFourthCount}회"
+        rank5thTxt.text = "5등 당첨 횟수 : ${mFifthCount}회"
+        loseTxt.text = "낙첨 횟수 : ${mLoseCount}회"
     }
 
 
